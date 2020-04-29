b3c1f8c972cbd1f569dbd03cf2cbeb1ab3acb07f
Glitch (xrgarden) <none>
2020-04-29 16:51:30 +0000

    👗🚕 Checkpoint ./index.js:300780/524 ./package.json:300780/1193

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
