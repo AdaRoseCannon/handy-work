d54078e1859e6a78ad07b845590f33ac3d46d6ef
Ada Rose Cannon <ada@ada.is>
2022-02-17 16:24:01 +0000

    esm standalone

--- rollup-standalone.config.js
+++ rollup-standalone.config.js
@@ -2,21 +2,21 @@ import replace from '@rollup/plugin-replace';
 import { execSync } from 'child_process';
 import { terser } from "rollup-plugin-terser";
 
-const handposeWorkerCompileCmd = `./node_modules/.bin/rollup -f cjs -p "rollup-plugin-terser" ./build/esm/handpose.js`;
+const handposeWorkerCompileCmd = `./node_modules/.bin/rollup -f esm -p "rollup-plugin-terser" ./build/esm/handpose.js`;
 const handposeSrc = '`\n' + execSync(handposeWorkerCompileCmd) + '`';
 
 export default {
 	input: "build/esm/handy-work.js",
 	output: {
-		format: "cjs",
+		format: "esm",
 		sourcemap: true,
-		file: 'build/cjs/handy-work.cjs.js',
+		file: 'build/esm/handy-work.standalone.js',
 	},
 	plugins: [
 		replace({
 			preventAssignment: false,
 			'var comlinkHandPose': function () {
-				return `const comlinkHandPose =  wrap(new Worker( new Worker(URL.createObjectURL( new Blob( [ ${ handposeSrc } ] ))) )); //`;
+				return `const comlinkHandPose =  wrap(new Worker( URL.createObjectURL( new Blob( [ ${ handposeSrc } ] )) )); //`;
 			}
 		}),
 		terser()
