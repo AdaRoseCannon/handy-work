2c5b7ba7f3c19767ee25d940d78f75743dc9d4eb
Ada Rose Cannon <ada@ada.is>
2022-02-22 13:23:18 +0000

    add aframe-to-md tool;

---
+++ aframe-to-md.mjs
@@ -0,0 +1,18 @@
+import tablemark from "tablemark";
+const myArgs = process.argv.slice(2);
+global.AFRAME= {
+	registerComponent: function (name, details) {
+		const table = [];
+		for (const [property, obj] of Object.entries(details.schema)) {
+			const out = {};
+			out.property = property;
+			Object.assign(out, obj);
+			out.type = out.type || typeof out.default;
+			out.description = out.description || "";
+			if (out.oneOf) out.description += ` One of ${out.oneOf.toString()}`;
+			table.push(out);
+		}
+		console.log(tablemark(table));
+	}
+}
+import(myArgs[0]);
